020aee5d8a3403e847a2fbff5440d54c0e9ae502
Rahul Gupta <coolrahulb1995@gmail.com>
2020-05-15 23:11:33 +0530

    Changes in the BookingApp and EventBookingApp

--- BookingApp/models.py
+++ BookingApp/models.py
@@ -15,14 +15,3 @@ class Bookingdb(models.Model):
     def __str__(self):
         return self.username
 
-# Class EventBookingdb(models.Model):
-#     username=models.CharField(max_length=100)
-#     email=models.EmailField(max_length=50)
-#     phone=models.IntegerField()
-#     age=models.IntegerField()
-#     proof=models.CharField(max_length=100)
-#     event=models.CharField(max_length=100)
-#     preferences=models.TextField(max_length=1000)
-
-#     def __str__(self):
-#         return self.username
